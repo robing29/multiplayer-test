8fa20868dcf23019516054d27497f66ddf908c89
robing29 <guderobin@gmail.com>
2020-05-26 22:44:40 +0200

    after first video-tutorial

---
+++ client/client.js
@@ -0,0 +1,29 @@
+const writeEvent = (text) => {
+    // <ul> element
+    const parent = document.querySelector('#events');
+
+    // <li> element
+    const el = document.createElement('li');
+    el.innerHTML = text;
+
+    parent.appendChild(el);
+};
+
+const onFormSubmitted = (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector('#chat');
+    const text = input.value;
+    input.value = '';
+
+    sock.emit('message', text);
+};
+
+writeEvent('Testing Clientsite');
+
+const sock = io();
+sock.on('message', writeEvent);
+
+document
+    .querySelector('#chat-form')
+    .addEventListener('submit', onFormSubmitted);
